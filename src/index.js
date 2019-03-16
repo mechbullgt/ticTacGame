7802253ee2daacdf6bee4164f35e4eabf4ffb594
mechbullgt <xyz.maqbool@gmail.com>
2019-03-17 01:09:11 +0530

    Added notes and completed the game logic.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,88 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+/* 
+Square Component for the square blocks on the board
+In Reacts term, the square component is a "controlled component" as being controlled by the Board component.
+*/
+/* class Square extends React.Component {
+    // Reacts way to remember things, this.state = (remember_this).
+    // Commented because Square class no longer stores the state.
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     };
+    // }
+
     render() {
         return (
-            <button className="square">
+            <button
+                className="square"
+                onClick={
+                    () => this.props.onClick()}>
                 {this.props.value}
             </button>
         );
     }
+} */
+
+// Updating the square component to a functional component as it has no state of its own.
+
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
+// Board component for the board which holds all the Squares
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    // Here we are taking a copy of the sqaures and mutating the data to maintain the changes of the object
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if(findingTheWinner(squares)||squares[i]){
+            return;
+        }
+        var xCurrentState = this.state.xIsNext;
+        squares[i] = xCurrentState ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !xCurrentState,
+        })
+    }
+
     renderSquare(i) {
         return (
-            <Square value={i} />
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
         );
     }
 
     render() {
-        const status = "Next Player: X";
+        console.log('this.state.squares:', this.state.squares);
+        //Finding the winner
+        const winner= findingTheWinner(this.state.squares);
+        let status;
+        if(winner){
+            status='Winner is: '+winner;
+        } else {
+            status = 'Next Player: '+(this.state.xIsNext?'X':'O');
+        }
         return (
             <div>
                 <div className="status">
@@ -46,12 +109,13 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component{
-    render(){
-        return(
+// Game Component that holds the Board and the board holds the squares
+class Game extends React.Component {
+    render() {
+        return (
             <div className="game">
                 <div className="game-board">
-                    <Board/>
+                    <Board />
                 </div>
                 <div className="game-info">
                     <div>
@@ -64,6 +128,34 @@ class Game extends React.Component{
     }
 }
 
+/*
+Function component to find the winner of the ticTac game
+*/
+function findingTheWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        console.log([a, b, c]);
+        console.log('a',squares[a]);
+        console.log('b',squares[b]);
+        console.log('c',squares[c]);
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 ReactDOM.render(
-    <Game/>, document.getElementById('root')
+    <Game />, document.getElementById('root')
 );
